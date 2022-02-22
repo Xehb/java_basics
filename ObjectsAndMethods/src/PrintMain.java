172f9ee2f716ad78a1b597bd5dad84e415598f8c
Xehb <xehb@list.ru>
2022-02-22 14:42:10 +0300

    homeworke 4.10.3/2

--- ObjectsAndMethods/src/PrintMain.java
+++ ObjectsAndMethods/src/PrintMain.java
@@ -2,16 +2,23 @@ public class PrintMain {
     public static void main(String[] args) {
         Printer doc1 = new Printer();
 
-    doc1.add("пробный принт", "текст какой-то", 6);
+        doc1.add("пробный принт", "текст какой-то", 6);
         doc1.appened();
-    doc1.print();
+        doc1.print();
 
         Printer doc2 = new Printer();
         doc2.add("Пробный принт 2", "Текст какой-то 2");
         doc2.appened();
         doc2.print();
-        doc1.clear();
-        doc1.print();
+
+        Printer doc3 = new Printer();
+
+        doc3.add("Тест какой-то 3");
+        doc3.appened();
+        doc3.print();
+
+//        doc1.clear();
+//        doc1.print();
     }
 }
 
--- ObjectsAndMethods/src/Printer.java
+++ ObjectsAndMethods/src/Printer.java
@@ -7,20 +7,19 @@ public class Printer {
     public void appened() {
         add(name, docText, pages);
 
-        queue =queue + this.name + "\n" +
-               this. docText + "\n" +
+        queue = queue + this.name + "\n" +
+                this.docText + "\n" +
                 "Колличество листов: " + this.pages;
-//        this.name = name;
-//        this.docText = docText;
-//        this.pages = pages;
-//    }
-//    public void add(String docText) {
-//        add(docText);
-////        this.docText = docText;
+    }
+    public void add( String docText) {
+        this.name = "Неизвестное имя";
+        this.docText = docText;
+        pages = 1;
+
     }
 
     public void add(String name, String docText) {
-        this.name =this.name +  name;
+        this.name = this.name + name;
         this.docText = docText;
         pages = 1;
 
@@ -28,10 +27,6 @@ public class Printer {
     }
 
     public void add(String name, String docText, int pages) {
-//        this.queue = queue + name + "\n" + docText + "\n" +
-//                "Колличество листов: " + pages;
-
-//        add(name,docText,pages);
         this.name = name;
         this.docText = docText;
         this.pages = pages;
