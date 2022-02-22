c8acd5aa023dfd607b5c33eaff04b0ad64cf77dc
Xehb <xehb@list.ru>
2022-02-22 16:15:46 +0300

    homeworke 4.10.3/3

--- ObjectsAndMethods/src/Printer.java
+++ ObjectsAndMethods/src/Printer.java
@@ -1,49 +1,42 @@
 public class Printer {
     public String queue = "";
-    public String name = "";
-    public int pages = 0;
-    public String docText = "";
 
-    public void appened() {
-        add(name, docText, pages);
 
-        queue = queue + this.name + "\n" +
-                this.docText + "\n" +
-                "Колличество листов: " + this.pages;
-    }
-    public void add( String docText) {
-        this.name = "Неизвестное имя";
-        this.docText = docText;
-        pages = 1;
+    public void appened(String name, String docText, int pages) {
+        boolean error = false;
+        if (containts(name)) {
+            error = true;
+        }
+        queue = queue + name + "\n" +
+                docText + "\n" +
+                "Колличество листов: " + pages +"\n";
 
     }
 
-    public void add(String name, String docText) {
-        this.name = this.name + name;
-        this.docText = docText;
-        pages = 1;
-
-
+    public void appened(String docText) {
+        appened("Неизвестнрое имя", docText, 1);
     }
 
-    public void add(String name, String docText, int pages) {
-        this.name = name;
-        this.docText = docText;
-        this.pages = pages;
-
-
+    public void appened(String name, String docText) {
+        appened(name, docText, 1);
     }
-
 
     public void clear() {
-        queue = "Получилось";
-//       this.name = "";
-//        this.docText = "";
-//        this.pages = 0;
-//        return;
+        queue = "Очищено";
     }
 
-    public void print() {
+    public boolean containts(String name) {
+        return queue.contains(name);
+    }
+
+    public void print(String title) {
+        System.out.println(title);
+        if (queue.isEmpty()) {
+            System.out.println("Нет печати");
+        } else {
+            System.out.println(queue);
+        }
+        clear();
         System.out.println(queue);
     }
 }
